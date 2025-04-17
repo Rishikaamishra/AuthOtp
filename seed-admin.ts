bf238835e2efae80556f91951ec28a2d74720e12
Rishika <rishika.mishra@systango.com>
2025-04-17 15:02:21 +0530

    changes in seeders

--- seed-admin.ts
+++ seed-admin.ts
@@ -6,18 +6,17 @@ import { User }        from './src/users/entities/user.entity';
 import { Role } from 'src/utils/common/enum/role.enum';
 
 async function bootstrap() {
-  // 1) Create an application context (so ConfigModule loads .env, TypeORM connects, etc)
+
   const app = await NestFactory.createApplicationContext(AppModule);
 
-  // 2) Grab the DataSource directly
   const ds = app.get(DataSource);
 
-  // 3) Get the User repository by the class, not by name
+  
   const repo = ds.getRepository(User);
 
   const adminEmail = 'rishika.mishra@systango.com';
 
-  // 4) Check for existing
+  
   const existing = await repo.findOne({ where: { email: adminEmail } });
   if (!existing) {
     const passwordHash = await bcrypt.hash('Admin@123', 10);
@@ -28,11 +27,11 @@ async function bootstrap() {
       role:      Role.ADMIN,
       isActive:  true,
       isDeleted: false,
-      // createdAt/updatedAt will be set automatically by @CreateDateColumn/@UpdateDateColumn
+      
     });
-    console.log('✅ Admin user created');
+    console.log('Admin user created');
   } else {
-    console.log('ℹ️  Admin user already exists');
+    console.log('ℹAdmin user already exists');
   }
 
   await app.close();
